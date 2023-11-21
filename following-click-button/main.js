21fef7ab5a2424c9dec23b4ccbe1c34b09ce9dd1
Fofo <127132293+fk-crafter@users.noreply.github.com>
2023-11-21 15:28:13 +0100

    Create main.js

---
+++ following-click-button/main.js
@@ -0,0 +1,18 @@
+function createRipple(event) {
+    const button = event.currentTarget;
+    const ripple = document.createElement("span");
+    const rect = button.getBoundingClientRect();
+    const size = Math.max(rect.width, rect.height);
+    
+    ripple.style.width = ripple.style.height = `${size}px`;
+    ripple.style.left = `${event.clientX - rect.left - size / 2}px`;
+    ripple.style.top = `${event.clientY - rect.top - size / 2}px`;
+  
+    ripple.classList.add("ripple");
+  
+    button.appendChild(ripple);
+    ripple.addEventListener("animationend", () => {
+      ripple.remove();
+    });
+  }
+  
